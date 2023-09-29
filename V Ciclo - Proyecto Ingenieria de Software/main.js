28f83e049c075107a0706eef5d6ec07a91693c69
Jennifer <114419997+Jennifer403-ship@users.noreply.github.com>
2023-09-29 12:35:27 -0500

    Add files via upload

---
+++ V Ciclo - Proyecto Ingenieria de Software/main.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector("#btn");
+let slidebar = document.querySelector(".slider-barra");
+let overlay = document.querySelector(".overlay");
+let isMenuActive = false;
+
+btn.onclick = function () {
+    slidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    isMenuActive = !isMenuActive;
+}
+
+overlay.onclick = function () {
+    if (isMenuActive) {
+        slidebar.classList.remove("active");
+        overlay.classList.remove("active");
+        isMenuActive = false;
+    }
+}
+
+searchbtn.onclick = function () {
+    slidebar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    isMenuActive = !isMenuActive;
+}
