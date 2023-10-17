ab0d5a44c6d2c8209c1d66054fe5662f4fff1868
Jennifer403-ship <jennifer15aries@gmail.com>
2023-10-17 12:53:34 -0500

    Ultima mod

---
+++ V Ciclo - Proyecto Ingenieria de Software/src/services/scrip.js
@@ -0,0 +1,4 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const chatbot = document.querySelector(".chatbot");
+
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot")); 
