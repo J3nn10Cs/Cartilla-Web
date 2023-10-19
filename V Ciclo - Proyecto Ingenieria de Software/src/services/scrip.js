ba0bc88ed960ca78c533dfc28313dfffd1befff6
Xpier0X <pieroleonyaipen07@gmail.com>
2023-10-18 20:07:50 -0500

    Actualizando el repositorio

--- V Ciclo - Proyecto Ingenieria de Software/src/services/scrip.js
+++
@@ -1,4 +0,0 @@
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const chatbot = document.querySelector(".chatbot");
-
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot")); 
